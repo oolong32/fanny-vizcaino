2d0a7b29f420f80ed76e98f7690376097f3b70e3
Coding for the Arts <88927553+cas-c4ta@users.noreply.github.com>
2021-12-19 15:25:31 +0100

    add english

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,8 +1,8 @@
 <footer>
   <p class="footer-title"><?= $site->title() ?></p>
   <p class="footer-address"> <?= $site->address()->html() ?><br>
-  <?= $site->zip()->html() ?> <?= $site->city()->html() ?><br>
-  <?= $site->country()->html() ?></p>
+  <?= $site->zip()->html() ?> <?= $site->city()->html() ?><!--<br>
+  <?= $site->country()->html() ?>--></p>
   <p class="footer-contact"><?= Html::mail($site->mail()) ?><br>
   Tel: <?= Html::tel($site->phone()) ?></p>
 </footer> 
